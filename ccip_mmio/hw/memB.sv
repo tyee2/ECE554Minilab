5cc2d174cc9088356c7ce92e09067be2468812cf
tyee2 <tyee2@wisc.edu>
2021-09-25 16:37:46 -0500

    edit memB
    needs testing. will also upload a simple tb

--- ccip_mmio/hw/memB.sv
+++ ccip_mmio/hw/memB.sv
@@ -8,3 +8,18 @@ module memB
     input signed [BITS_AB-1:0] Bin [DIM-1:0],
     output signed [BITS_AB-1:0] Bout [DIM-1:0]
     );
+
+	genvar i;
+	generate
+		for(i=0; i<DIM; ++i) begin
+			fifo #(DIM,BITS_AB) fifo_memB(
+				.clk(clk),
+				.rst_n(rst_n),
+				.en(en),
+				.d(Bin),
+				.q(Bout[i])
+			);
+		end
+	endgenerate
+	
+endmodule
